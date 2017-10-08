7f4af942f6ee36fc3da3a925096d4cc6fe208227
eric <eric.liu@bntake.com>
2017-10-08 17:34:56 +1100

    add test  function 3 in  content script for click

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -8,11 +8,17 @@ console.log('\'Allo \'Allo! Content script');
 function test1(){
   var div=document.createElement("div");
   document.body.appendChild(div);
-  div.innerText="great eric add";
+  div.id = "ep_pl";
+  div.style.cssText="    position: fixed;right: 0;display: block;z-index: 888;bottom: 0;";
+  div.innerText="great eric add ";
+  div.onclick = function () {
+    alert("helo!");
+  };
+
   console.log('add new ');
 }
 
-// test1();
+test1();
 //-----------
 
 var timer = null;
@@ -51,3 +57,41 @@ function clearTimer() {
 
 // test2();   //works
 //-----------
+
+function test3(){
+  var planets = document.getElementById("ep_pl"),
+    randomplanet = Math.floor(Math.random() * planets.length);
+    // clickplanet = planets[randomplanet];
+
+    // planets.click();
+
+    planets.innerHTML = "fffff new from test 3<br/>adsfasdf</br>asdfsadf<br/>fasdf";
+    console.log("click planets");
+
+    var bodyRect = document.body.getBoundingClientRect(),
+    elemRect = planets.getBoundingClientRect(),
+    offset   = elemRect.top - bodyRect.top;
+    var offset_left = elemRect.left - bodyRect.left;
+      console.log('Element is ' + offset + ' vertical pixels from <body>');
+      console.log('Element is ' + offset_left + ' left pixels from <body>');
+    //click( offset + 50, offset_left+50);
+    // document.elementFromPoint(offset+4, offset_left+4).click();
+    planets.click();  //works
+}
+// test3();    //with test1
+
+function click(x,y){
+  console.log("start click");
+    var ev = document.createEvent("MouseEvent");
+    var el = document.elementFromPoint(x,y);
+
+    ev.initMouseEvent(
+        "click",
+        true /* bubble */, true /* cancelable */,
+        window, null,
+        x, y, 0, 0, /* coordinates */
+        false, false, false, false, /* modifier keys */
+        0 /*left*/, null
+    );
+    // el.dispatchEvent(ev);
+}
