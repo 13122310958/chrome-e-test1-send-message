0d11f49876d86d0d0924d4504a62d607727ea2ee
eric <eric.liu@bntake.com>
2017-10-08 15:53:53 +1100

    add test  function 4 in  backgroud js ,

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,6 +4,8 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
+//reload plugin then run here. in backgroud
+
 chrome.browserAction.setBadgeText({text: '\'gleo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
@@ -19,9 +21,6 @@ chrome.tabs.executeScript({
     code: 'console.log("from background")'
 });
 
-window.setInterval("setNumber",1000);
-
-
 chrome.runtime.onMessage.addListener (function (request, sender, sendResponse) {
     // alert("Contents Of Text File = " + request.fileData);
     //  console.log(JSON.stringify(request));
@@ -33,8 +32,56 @@ chrome.runtime.onMessage.addListener (function (request, sender, sendResponse) {
      chrome.tabs.executeScript({
          code: 'console.log(\' --- '+JSON.stringify(request)+'\')'
      });
+
      var i = new Date();
       sendResponse({options: 1,d:2});
       // sendResponse('ok');  //works
      return true;
 });
+
+
+
+var timer = null;
+var waitTimer = null;
+var interval = -1;
+var secondsToWait = -1;
+var countdownDom = null;
+var options = null;
+var notificationId = null;
+var badgeAdded = false;
+
+function test4(){
+  //当 popup page display then run
+  console.log("test test 4");
+  output("start test 4");
+  interval = 5;
+  timer = setInterval(function () {
+               interval -= 1;
+               showCountDown(interval);
+               if (interval <= 0) {
+                   //clearTimer();
+                  test4_timer();
+                    interval = 5;
+               }
+           }, 1000);
+
+}
+function showCountDown(i){
+    console.log(i);
+    output(i);
+}
+function test4_timer(){
+    output("start test 4 go ... ");
+}
+function clearTimer() {
+    if (!timer) return;
+    clearInterval(timer);
+    timer = null;
+}
+
+function output(data){
+  chrome.tabs.executeScript({
+    code: 'console.log(\''+data+'\')'
+  });
+}
+// test4();  //works
