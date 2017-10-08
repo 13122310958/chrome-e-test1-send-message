30870590b8540c0722428601377c090e36fd6071
eric <eric.liu@bntake.com>
2017-10-08 15:35:45 +1100

    add text  function in page

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -5,9 +5,11 @@ console.log('\'Allo \'Allo! Content script');
 // 这里是主要的 主体。
 // 一段注入到页面文件中的JavaScript代码
 //
-var div=document.createElement("div");
-document.body.appendChild(div);
-div.innerText="great eric add";
+function test1(){
+  var div=document.createElement("div");
+  document.body.appendChild(div);
+  div.innerText="great eric add";
+  console.log('add new ');
+}
 
-
-console.log('add new ');
+test1();
